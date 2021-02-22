d48dd52fd83794561bdd0f9fb7dbed952a1e4d5c
nick <nicholas_scalione@yahoo.com>
2021-02-21 23:31:47 -0500

    commit tree 6156c7e852a12455a8969869e81d040db53217db

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,29 @@
-class Application
 
+class Application
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
+    if req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+    elsif req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/add/)
+      search_ter = req.params["item"]
+      resp.write add_item(search_ter)
     elsif req.path.match(/search/)
+      binding.pry
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     else
@@ -20,11 +33,19 @@ class Application
     resp.finish
   end
 
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
+  def add_item(search_ter)
+    if @@items.include?(search_ter)
+        @@cart << search_ter
+          return "added #{search_ter}"
     end
+    return "We don't have that item"
   end
+
+    def handle_search(search_term)
+      if @@items.include?(search_term)
+        return "#{search_term} is one of our items"
+      else
+        return "Couldn't find #{search_term}"
+      end
+    end
 end
